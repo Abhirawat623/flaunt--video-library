4ea8b455bde3098bd7d6096e34e4d18bac244fe2
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-02-07 22:06:28 +0530

    signup comp modals logic added

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,76 +1,104 @@
+import { useModal } from "../context/modals-context";
+export const SignUp = () => {
+  //to close signup modal
+  const { hideModal } = useModal();
 
-export const SignUp=()=>{
+  const handleSignupModalClose = () => {
+    hideModal();
+  };
 
-   return(
-    <div className="bg-black p-2 xl:w-96 xl:h-96 h-96 w-80 mx-auto ">
-        <div className=" flex flex-col items-center">
-          <h2 className="text-white xl:font-bold font-bold ">Sign Up</h2> </div>
-          <form
-            // onSubmit={handleFormSubmit}
-            className="flex flex-col items-center "  > 
-              <div className=" flex flex-col items-center ">
-                  <label className="text-white ml-l">Name*</label>
-                  <input
-                    className="input-form p-0.5 rounded-md "
-                    placeholder="Enter Full Name"
-                    required
-                    // defaultValue={username}
-                    // onChange={handleNameSignupChange}
-                  />
-                </div>
-                <div className="flex flex-col items-center">
-                  <label className="flex flex-col text-white ml-l">E-mail*</label>
-                  <input
-                    className=" p-0.5 rounded-md"
-                    placeholder="Enter a valid E-mail"
-                    required
-                    // defaultValue={email}
-                    // onChange={handleEmailSignupChange}
-                  />
-                </div>
-                <div className="flex flex-col items-center">
-                  <label className=" text-white ml-l">
-                    Password*
-                  </label>
-                  <input
-                    className=" p-0.5 rounded-md"
-                    placeholder="Must be 8 letters including caps letter & special character"
-                    required
-                    type="password"
-                    // onChange={handlePasswordSignupChange}
-                    // defaultValue={password}
-                  />
-                </div>
-                <div className="flex flex-col items-center">
-                  <label className=" text-white ml-l">
-                    Confirm Password*
-                  </label>
-                  <input
-                    className="p-0.5 rounded-md"
-                    placeholder="Must be 8 letters including caps latter & special character"
-                    required
-                    type="password"
-                    // onChange={handleConfirmPasswordSignupChange}
-                    // defaultValue={confirmPassword}
-                  />
-                </div>
-                <div className="flex flex-col items-center">
-                <label className="text-white  ">
-                  Mobile Number*
-                </label>
-                <input
-                  className=" p-0.5 rounded-md"
-                  placeholder="Enter Mobile Number"
-                  maxLength={10}
-                  required
-                //   defaultValue={number}
-                //   onChange={handleMobileSignupChange}
-                />
-                </div>
-                <button className="text-black bg-purple-200">Create Account</button>
-                </form>
-         
-          
+  return (
+    <div
+      className="bg-black p-6 xl:w-96 xl:h-102 h-100 w-80 
+    mx-auto flex flex-col items-center justify-between rounded-lg relative"
+    >
+      <div className=" flex flex-col items-center">
+        <h2 className="text-white text-2xl font-bold ">Sign Up</h2>{" "}
+        <span
+          className="text-white text-4xl absolute top-1 right-1 cursor-pointer"
+          onClick={handleSignupModalClose}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-10 h-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </span>
       </div>
-    )
-}
+      <form
+        // onSubmit={handleFormSubmit}
+        className="flex flex-col items-center gap-y-2 xl:text-xl"
+      >
+        <div className=" flex flex-col items-center ">
+          <label className="text-white ml-l">Name*</label>
+          <input
+            className="p-0.5 rounded-md xl:w-80 w-72 cursor-pointer "
+            placeholder="Enter Full Name"
+            required
+            // defaultValue={username}
+            // onChange={handleNameSignupChange}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <label className="flex flex-col text-white ml-l">E-mail*</label>
+          <input
+            className=" p-0.5 rounded-md xl:w-80 w-72 cursor-pointer"
+            placeholder="Enter a valid E-mail"
+            required
+            // defaultValue={email}
+            // onChange={handleEmailSignupChange}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <label className=" text-white ml-l">Password*</label>
+          <input
+            className=" p-0.5 rounded-md xl:w-80 w-72 cursor-pointer"
+            placeholder="Must be 8 letters including caps letter & special character"
+            required
+            type="password"
+            // onChange={handlePasswordSignupChange}
+            // defaultValue={password}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <label className=" text-white ml-l">Confirm Password*</label>
+          <input
+            className="p-0.5 rounded-md xl:w-80 w-72 cursor-pointer"
+            placeholder="Must be 8 letters including caps latter & special character"
+            required
+            type="password"
+            // onChange={handleConfirmPasswordSignupChange}
+            // defaultValue={confirmPassword}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <label className="text-white  ">Mobile Number*</label>
+          <input
+            className=" p-0.5 rounded-md xl:w-80 w-72 cursor-pointer"
+            placeholder="Enter Mobile Number"
+            maxLength={10}
+            required
+            //   defaultValue={number}
+            //   onChange={handleMobileSignupChange}
+          />
+        </div>
+      </form>
+      <button
+        className=" text-white bg-green-800 xl:w-80 w-72 p-0.5 
+                 hover:bg-green-600 hover:text-white h-10 rounded-md
+                 text-base xl:text-lg cursor-pointer"
+      >
+        Create Account
+      </button>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: [ "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '102': '30rem',
+        '100': '28rem',
+      },
+      width: {
+        '58': '12.6rem',
+      }
+    },
   },
   plugins: [],
 }
